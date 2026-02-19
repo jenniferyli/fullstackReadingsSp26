e6cf3f167e67337034d52bed1a4c3a699a52343a
jenniferyli <jennifer.yuzhen.li@gmail.com>
2026-02-19 11:33:53 -0500

    brick-14: Create brick grid and integrate into game loop

--- brick-breaker-game/src/main.js
+++ brick-breaker-game/src/main.js
@@ -1,9 +1,28 @@
 import "./style.css";
 import Ball from "./model/ball.js";
+import Brick from "./model/brick.js";
+
 import Paddle from "./model/paddle.js";
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const bricks = [];
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+for (let c = 0; c < brickColumnCount; c++) {
+  for (let r = 0; r < brickRowCount; r++) {
+    let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+    let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+    bricks.push(new Brick(brickX, brickY, brickWidth, brickHeight, "#0095DD"));
+  }
+}
 
 const ball = new Ball(
   canvas.width / 2,
@@ -31,6 +50,11 @@ function draw() {
   ball.move();
   // ball.bounce(canvas.width, canvas.height);
   isGameOver = !ball.bounce(canvas.width, canvas.height);
+
+  bricks.forEach((brick) => {
+    brick.draw(ctx);
+    brick.collides(ball);
+  });
 
   paddle.draw(ctx);
   paddle.move(canvas.width);
