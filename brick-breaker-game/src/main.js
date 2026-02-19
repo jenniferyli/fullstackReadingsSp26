8e5e6ee599e1985614cfc73a4c073c3a1990d918
jenniferyli <jennifer.yuzhen.li@gmail.com>
2026-02-19 10:59:13 -0500

    brick-06: Use Sprite class in main animation loop

--- brick-breaker-game/src/main.js
+++ brick-breaker-game/src/main.js
@@ -1,29 +1,29 @@
 import "./style.css";
 import Block from "./model/block.js";
+import Sprite from "./model/sprite.js";
+
 
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50); // x, y, width, height
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-
-
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-const dx = 2;
-const dy = -2;
+const redBlock = new Block(20, 40, 50, 50, "#FF0000");
+const blueSprite = new Sprite(
+   canvas.width / 2,
+   canvas.height - 30,
+   10,
+   10,
+   "#0095DD",
+   2,
+   -2
+);
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);   
-    const redBlock = new Block(20, 40, 50, 50, "#FF0000");
-    redBlock.draw(ctx);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);  
 
-    x += dx;
-    y += dy;
+    redBlock.draw(ctx);
+    blueSprite.draw(ctx);
+    blueSprite.move();
 
     window.requestAnimationFrame(draw);
 }
