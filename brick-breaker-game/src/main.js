60accf7191c201bd37697338ad1c7ce51390ee61
jenniferyli <jennifer.yuzhen.li@gmail.com>
2026-02-19 11:05:58 -0500

    brick-09: Add keyboard-controlled paddle

--- brick-breaker-game/src/main.js
+++ brick-breaker-game/src/main.js
@@ -1,5 +1,7 @@
 import "./style.css";
 import Ball from "./model/ball.js";
+import Paddle from "./model/paddle.js";
+
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
@@ -14,14 +16,27 @@ const ball = new Ball(
   -2,
 );
 
+const paddle = new Paddle(
+  (canvas.width - 75) / 2,
+  canvas.height - 10,
+  75,
+  10,
+  "#0095DD",
+);
+
+
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ball.draw(ctx);
-  ball.move();
-  ball.bounce(canvas.width, canvas.height);
+    ball.draw(ctx);
+    ball.move();
+    ball.bounce(canvas.width, canvas.height);
 
-  window.requestAnimationFrame(draw);
+    paddle.draw(ctx);
+    paddle.move();
+
+
+    window.requestAnimationFrame(draw);
 }
 
 draw();
--- brick-breaker-game/src/model/ball.js
+++ brick-breaker-game/src/model/ball.js
@@ -6,13 +6,11 @@ class Ball extends Sprite {
 //   }
 
     bounce(canvasWidth, canvasHeight) {
-       if (this.x < 0 || this.x > canvasWidth) {
        if (this.x < 0 || this.x + this.width > canvasWidth) {
         // bounce off the left/right edges
             this.dx *= -1; // switch direction
         }
 
-        if (this.y < 0 || this.y > canvasHeight) {
         if (this.y < 0 || this.y + this.height > canvasHeight) {
         // bounce off the top/bottom edge
             this.dy *= -1; // switch direction
