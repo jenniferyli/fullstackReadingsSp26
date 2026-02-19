929c52b0e9be0e8745544c819a8b3953751ecaf9
jenniferyli <jennifer.yuzhen.li@gmail.com>
2026-02-19 11:14:37 -0500

    brick-10: Clamp paddle movement to canvas bounds

--- brick-breaker-game/src/main.js
+++ brick-breaker-game/src/main.js
@@ -33,7 +33,7 @@ function draw() {
     ball.bounce(canvas.width, canvas.height);
 
     paddle.draw(ctx);
-    paddle.move();
+    paddle.move(canvas.width);
 
 
     window.requestAnimationFrame(draw);
