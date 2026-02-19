e89957d929ecf127cf21e3156bbb00451957dd10
jenniferyli <jennifer.yuzhen.li@gmail.com>
2026-02-19 11:03:23 -0500

    brick-08: Refine ball bounce logic and fix edge collision

--- brick-breaker-game/src/model/ball.js
+++ brick-breaker-game/src/model/ball.js
@@ -1,23 +1,24 @@
 import Sprite from "./sprite.js";
 
 class Ball extends Sprite {
-  constructor(x, y, width, height, color, dx, dy) {
-    super(x, y, width, height, color, dx, dy);
-  }
+//   constructor(x, y, width, height, color, dx, dy) {
+//     super(x, y, width, height, color, dx, dy);
+//   }
 
     bounce(canvasWidth, canvasHeight) {
-    if (this.x < 0) {
-        this.dx *= -1; // 👀 reverse horizontal direction
-    } else if (this.x > canvasWidth) {
-        this.dx *= -1;
+       if (this.x < 0 || this.x > canvasWidth) {
+       if (this.x < 0 || this.x + this.width > canvasWidth) {
+        // bounce off the left/right edges
+            this.dx *= -1; // switch direction
+        }
+
+        if (this.y < 0 || this.y > canvasHeight) {
+        if (this.y < 0 || this.y + this.height > canvasHeight) {
+        // bounce off the top/bottom edge
+            this.dy *= -1; // switch direction
+        }
     }
 
-    if (this.y < 0) {
-        this.dy *= -1; // 👀 reverse vertical direction
-    } else if (this.y > canvasHeight) {
-        this.dy *= -1;
-    }
-    }
 
 
 }
