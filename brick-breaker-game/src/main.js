a1c9b3b58637fc2856c456e8ba0990943588f36b
jenniferyli <jennifer.yuzhen.li@gmail.com>
2026-02-19 11:01:40 -0500

    brick-07: Add Ball class with edge bouncing

--- brick-breaker-game/src/main.js
+++ brick-breaker-game/src/main.js
@@ -1,31 +1,27 @@
 import "./style.css";
-import Block from "./model/block.js";
-import Sprite from "./model/sprite.js";
-
-
+import Ball from "./model/ball.js";
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const redBlock = new Block(20, 40, 50, 50, "#FF0000");
-const blueSprite = new Sprite(
-   canvas.width / 2,
-   canvas.height - 30,
-   10,
-   10,
-   "#0095DD",
-   2,
-   -2
+const ball = new Ball(
+  canvas.width / 2,
+  canvas.height - 30,
+  10,
+  10,
+  "#0095DD",
+  2,
+  -2,
 );
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    redBlock.draw(ctx);
-    blueSprite.draw(ctx);
-    blueSprite.move();
+  ball.draw(ctx);
+  ball.move();
+  ball.bounce(canvas.width, canvas.height);
 
-    window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(draw);
 }
 
 draw();
